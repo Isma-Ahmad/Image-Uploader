00e356a287a4add5caf080cf416b5463f3db31a8
ismaahmadmad <ismaa7990@gmail.com>
2024-08-11 15:15:21 +0500

    Image Uploader

--- services/imageService.js
+++ services/imageService.js
@@ -2,7 +2,7 @@ const { Image } = require('../models/index');
 const fs = require('fs');
 
 const uploadImage = async (filePath) => {
-    try {
+
         
         const imageData = fs.readFileSync(filePath);
         if (!imageData) {
@@ -11,35 +11,28 @@ const uploadImage = async (filePath) => {
        
         const image = await Image.create({ image: imageData });
         return image;
-    } catch (error) {
-        throw new Error('Error uploading image: ' + error.message);
-    }
+   
 };
 
 const getImageById = async (id) => {
-    try {
+
         const image = await Image.findByPk(id);
         if (!image) {
             throw new Error('Image not found');
         }
         return image;
-    } catch (error) {
-        throw new Error('Error retrieving image: ' + error.message);
-    }
+   
 };
 
 const deleteImageById = async (id) => {
-    try {
+
         const image = await Image.findByPk(id);
         if (!image) {
             throw new Error('Image not found');
         }
-
         await image.destroy();
-        return image;
-    } catch (error) {
-        throw new Error('Error deleting image: ' + error.message);
-    }
+        return { message: 'Image deleted successfully' };
+
 };
 
 
